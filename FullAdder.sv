adcc3364737cec50962ebb4af3f4ca61c221c77d
Enrique Orozco Jr. <enrique-orozco@outlook.com>
2024-07-14 20:37:37 -0500

    Difficulty - Easy

---
+++ FullAdder.sv
@@ -0,0 +1,12 @@
+module model (
+  input a,
+  input b,
+  input cin,
+  output logic sum,
+  output logic cout
+);
+
+  assign sum = (a ^ b) ^ cin;
+  assign cout = ((a ^ b) & cin) | (a & b);
+
+endmodule
